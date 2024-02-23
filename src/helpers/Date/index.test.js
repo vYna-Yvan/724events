6022f024241f3f11d061204979ef44ef9601d591
vYna-Yvan <89458360+vYna-Yvan@users.noreply.github.com>
2024-02-23 21:18:24 +0100

    date test file Helper

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -10,9 +10,17 @@ describe("Date helper", () => {
       const month = getMonth(new Date("2022-01-01"));
       expect(month).toBe("janvier");
     });
+    it("the function return mars for 2022-03-29 as date", () => {
+      const month = getMonth(new Date("2022-03-29"));
+      expect(month).toBe("mars"); // test ajouter
+    });
     it("the function return juillet for 2022-07-08 as date", () => {
       const month = getMonth(new Date("2022-07-08"));
-      expect(month).toBe("juillet");
+      expect(month).toBe("juillet"); // test ajouter
+    });
+    it("the function return août 2022-08-29 as date", () => {
+      const month = getMonth(new Date("2022-08-29"));
+      expect(month).toBe("août"); // test ajouter
     });
   });
 });
